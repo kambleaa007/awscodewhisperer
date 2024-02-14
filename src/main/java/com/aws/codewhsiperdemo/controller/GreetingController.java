7ae0c75480e568dd12db71c5ec6b9d9ef25f8dad
kambleaa007 <ashishkamble191191@gmail.com>
2024-02-14 14:59:43 +0530

    security added

--- src/main/java/com/aws/codewhsiperdemo/controller/GreetingController.java
+++ src/main/java/com/aws/codewhsiperdemo/controller/GreetingController.java
@@ -1,12 +1,35 @@
 package com.aws.codewhsiperdemo.controller;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class GreetingController {
 
+
+	@RequestMapping("/")
+	public ModelAndView index(Model model) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		ModelAndView modelAndView = new ModelAndView();
+    	
+
+		if (!(auth instanceof AnonymousAuthenticationToken))
+		{
+			modelAndView.setViewName("success.html");
+			return modelAndView;
+		}
+		modelAndView.setViewName("error.html");
+		// if it is not authenticated, then go to the error...
+
+		return modelAndView;
+	}
 
 	@GetMapping("/greeting")
 	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
---
+++ src/main/java/com/aws/codewhsiperdemo/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.aws.codewhsiperdemo.security;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+// import org.springframework.security.core.userdetails.UserDetailsService;
+
+// // spring boot security config file
+// @Configuration
+// @EnableWebSecurity
+// public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//     @Autowired
+//     private UserDetailsService userService;
+
+    
+//     protected void configure(HttpSecurity http) throws Exception {
+//         // the boolean flags force the redirection even though 
+//         // the user requested a specific secured resource.
+//         http.formLogin().defaultSuccessUrl("/success.html", true);
+//     }
+
+//     @Autowired
+//     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//         auth.userDetailsService(userService);
+//     }
+// }
